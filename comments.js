b26d3ec62ced8df6fd32790b2a71d25ff5620537
ikibkilam <bikimalik@hotmail.com>
2019-03-03 21:27:45 -0800

    Added PATCH /todo route

--- comments.js
+++ comments.js
@@ -1,9 +1,33 @@
-// Remove documents.
+// Update documents.
 
-// 1. I setup the code for Todo.remove() and found that this is deprecated.
-// 2. I setup the code for Todo.deleteOne() and found that as in Todo.remove
-//    it returns and object that shows how many documents were deleted.
-// 3. I setup the code for findByIdAndDelete() and found that this actually
-//    returns the deleted document. This is nice.
-// 4. I then moved to server.js and wrote a handler for a delete operation
-//    based on this learning.
+// 1. I installed lodash.
+// 2. In app.patch route, used to update values, I got the ID.
+// 3. I used the .pick() method to extract two properties in the request object. Note,
+//    the request object contains three properties - text, completed and completedAt.
+//    We do not want to extract completedAt, since this is not to be filled by the user,
+//    but by the server app, and we do this below. Text and completed will be filled by
+//    user, presumably on the client (and we simulate this in Postman). See the format
+//    for the .pick() method in the lodash docs, and note it returns an object with the
+//    extracted properties.
+// 4. Next, I check if the ID is valid, as before.
+// 5. Then, I update the completedAt property, based on the request object properties,
+//    text and completed. If the complete property is boolean and is true, then we 
+//    update the completedAt property, and if not, then we do not. Note, since completedAt
+//    is a type number, we just need to initialize it to a unique number, if the complete
+//    property is true. And we need to initialize it to null if complete is false.
+// 6. Use findByIdAndUpdate() method, which has the format (id, update object, options).
+//    The update object is the object that we want to update, and it could be an object
+//    with the property being updated, or, properties being updated. Options is an object
+//    with some parameters, such as new, which returns the modified document, instead of
+//    the default original document. See documentation on this method in mongoose. I did
+//    check if there is any document returned (ie. did ID exist), and if not, I just 
+//    returned an error. Also, in the catch statement, I return a 400 error. 
+// 7. Note, error 400 says, the server will not process request because client request was bad
+//    (malformed request syntax, invalid request framing etc.). The 404 error says, client
+//    was able to communicate with the server, but the server could not find the what was
+//    requested.
+// 8. I tested in Postman. Note, the PATCH http verb and request is all about providing the
+//    the data, and so we provide this body in the form of JSON, using the upper half of the
+//    Postman screen. The lower half of the screen shows me the response object.
+// 9. Since all files are being tracked but have not been updated, I used git commit -a -m "",
+//    to commit the changes to git.
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 // Import mongodb
 const {ObjectID} = require('mongodb');
+// Import lodash
+const _ = require('lodash');
 
 // 2. Import custom modules.
 const {Todo} = require('./models/todo.js');
@@ -87,6 +89,37 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((error) => {
         //Failure. Return 400 for example, if server is down.
     res.status(400).send();
+    });
+});
+
+// 9. Update a todo.
+app.patch('/todos/:id', (req, res) => {
+    // Get the ID.
+    var id = req.params.id;
+    // Pick the text and completed properties (key-value pairs) from the request object.
+    var body = _.pick(req.body, ['text', 'completed']);
+    console.log('body: ', body);
+    console.log('req.body: ', JSON.stringify(req.body, undefined, 2)); // I stringiifed this to show that the req object is a JS object.
+    // Validate ID. If not valid return 404.
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    // Update the value of property completedAt, based on the value of property completed.
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completedAt = null;
+    }
+    // Update the values of the properties completed and text, based on user request.
+    Todo.findByIdAndUpdate(id, body, {new: true}).then((todo) => {
+        // Check if ID is returned and if not then return error.
+        if (!todo) {
+            return res.status(404),send();
+        }
+        // Return modified todo.
+        res.send(todo);
+    }).catch((error) => {
+        res.status(400).send();
     });
 });
 
